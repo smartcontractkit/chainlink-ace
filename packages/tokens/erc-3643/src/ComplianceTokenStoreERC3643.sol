874a86b5b0b8c69004bf1e02e5414f9f5b4d6094
Uriel Kelman <urielkelman@gmail.com>
2025-10-22 16:29:21 -0300

    Release - 2025-10-22 16:29:21

--- packages/tokens/erc-3643/src/ComplianceTokenStoreERC3643.sol
+++ packages/tokens/erc-3643/src/ComplianceTokenStoreERC3643.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.26;
 
 contract ComplianceTokenStoreERC3643 {
-  /// @custom:storage-location erc7201:compliance-token-erc20.ComplianceTokenStoreERC3643
+  /// @custom:storage-location erc7201:compliance-token-erc3643.ComplianceTokenStoreERC3643
   struct ComplianceTokenStorage {
     string tokenName;
     string tokenSymbol;
@@ -15,11 +15,11 @@ contract ComplianceTokenStoreERC3643 {
     mapping(address userAddress => uint256 amount) frozenTokens;
   }
 
-  // keccak256(abi.encode(uint256(keccak256("compliance-token-erc20.ComplianceTokenStoreERC3643")) - 1)) &
+  // keccak256(abi.encode(uint256(keccak256("compliance-token-erc3643.ComplianceTokenStoreERC3643")) - 1)) &
   // ~bytes32(uint256(0xff))
   // solhint-disable-next-line const-name-snakecase
   bytes32 private constant complianceTokenStorageLocation =
-    0xb812bfd341d808811ca5cb8ca1c12a45441244343a7b7b1ef61042686f756b00;
+    0xdc918d90baf191b8d972d59f66f8d1c2691d3df52961983ba712e24ad9fcd600;
 
   function getComplianceTokenStorage() internal pure returns (ComplianceTokenStorage storage $) {
     // solhint-disable-next-line no-inline-assembly
